7ccb615116d782c7a906a89f6f3689c64a8f8048
bhernan13 <49212248+bhernan13@users.noreply.github.com>
2019-04-12 16:24:57 -0400

    Add files via upload
    added new conversion function and streamlined inputs for ease of use in main script

--- Functions/pressuredrop.m
+++ Functions/pressuredrop.m
@@ -1,34 +1,47 @@
-function dp = pressuredrop(mdot,porosity,dpipe,dpeb,dt,velocity,density_eth,viscosity_eth,npeb)
+function dp = pressuredrop(mdot,IDpipe,dpeb,press,temp,fluid)
 
 % Created: February 17, 2019
 % Code Author: Madie Melcer
+% Modified: Brian Hernan April 12, 2019
+% Streamlined inputs for use in main pebble bed analysis loop.
+% Removed 'global conv' conversions and switched to convert.m
+% Call pebpack within loop for consistency and fewer inputs
+%
 % Code to calculate the pressure drop across a slice of our pebble beds
 % given inputs of mass flow rate, porosity, pipe diameter, pebble diameter,
 % time step, ethane velocity, ethane density, ethane viscosity, and number
 % of pebbles per layer. Uses pressure drop correlation from paper 'Analysis of Pressure
 % Drop and Heat Transfer of a Pebble-Bed-Storage Heater for a Hypersonic
 % Wind Tunnel' by D. E. Randall and A. Bedford in 1961 from Sandia. 
-% Inputs are in metric units.
-%% Load conversions
-global conv
-
-%% setup
-D = dpipe/conv.ft2m; % ft
-d = dpeb/conv.ft2m; % ft
-rho_eth = density_eth*conv.kgm32lbft3; % lb/ft^3
-mu_eth = viscosity_eth*conv.Pas2lbfts; % lb/ft-s
+% Inputs:
+% mdot - massflow, kg/s
+% porisity - portion of pipe not filled with pebbles, void volume/volume
+% dpipe - Inner Pipe Diameter, in
+% dpeb - Pebble Diameter, in
+% Press - Local Pressure, Pa
+% Temp - Local Temperature, K
+% fluid - Refpropm library string of fluid and .mix or .fld suffix
+%
+% Outputs:
+% dp - Pressure drop in psi over a given section of the bed
+%
+%% Setup
 g = 32.2; % ft/s^2
+IDpipe = convert.length(IDpipe,'m','ft');
+dpeb = convert.length(dpeb,'m','ft'); % ft
+rho_eth = convert.density(refpropm('D','T',temp,'P',convert.pressure(press,'pa','kPa'),fluid),'kg/m3','lbm/ft3');
+mu_eth = convert.dynamicvisc(refpropm('V','T',temp,'P',convert.pressure(press,'pa','kPa'),fluid),'Pa-s','lb/ft-s');
+w = convert.mass(mdot,'kg','lbm');
 
-%% calculations
-dx = dpeb; % ft
-A = pi/4*D^2; % ft^2
-w = mdot/conv.lbm2kg; % lb/s, mass flow rate
-U = velocity/conv.ft2m; % ft/s
-vol_bed = pi*(D/2)^2*dx; % for this slice, ft^3
-npeb_in_slice = npeb*(dx/d);
-S = npeb_in_slice*4*pi*(d/2)^2/vol_bed; % 1/ft, area of pebbles per unit 
-    % volume of bed
-Re2 = rho_eth*U/(mu_eth*S); % Reynolds number for pebble bed based on 
-    % pebble surface area per unit volume of bed
-dp = 2.4*(Re2^-0.1)*((1-porosity)/porosity)*(w^2/(rho_eth*g*d*(A*porosity)^2)*dx);
+%% Calculations
+dx = dpeb;
+A = pi/4*IDpipe^2; % ft^2
+U =  w / (rho_eth*A); % ft/s
+[npeb_in_slice, porosity, ~ , vol_bed] = pebpack(dpeb,IDpipe);
+% 1/ft, area of pebbles per unit volume of bed
+S = npeb_in_slice*4*pi*(dpeb/2)^2/vol_bed; 
+% Reynolds number for pebble bed based on pebble surface area per unit volume of bed
+Re2 = rho_eth*U/(mu_eth*S); 
+% Pressure drop over given section
+dp = 2.4*(Re2^-0.1)*((1-porosity)/porosity)*(w^2/(rho_eth*g*dpeb*(A*porosity)^2)*dx);
 
